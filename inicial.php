2e573789930b77327d40bad3298f94827dc0cedf
Eduardomassinhani <eduardo.massinhani@gmail.com>
2016-07-26 23:17:35 -0300

    Add files via upload

---
+++ inicial.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="windows-1252">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $nome = "Eduardo Massinhani";
+        echo $nome;
+        ?>
+    </body>
+</html>
